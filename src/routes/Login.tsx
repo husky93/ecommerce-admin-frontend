efcb1a9a6c20f3b1c79e598ce51237ca06a2e708
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-17 19:18:53 +0100

    Create basic Login page template

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import styles from '../../assets/styles/login/Login.module.css';
+import styles from '../assets/styles/login/Login.module.css';
 
 interface LoginProps {}
 
 const Login: React.FC<LoginProps> = ({}) => {
-  return <></>;
+  return (
+    <main className={styles.main}>
+      <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="username">E-Mail:</label>
+        <input
+          id="username"
+          name="username"
+          type="email"
+          placeholder="example@website.com"
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="example@website.com"
+        />
+      </form>
+    </main>
+  );
 };
 
 export default Login;
