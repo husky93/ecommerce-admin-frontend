b0946736a55beec598b811c618f0aba1677d7caf
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-17 15:08:24 +0100

    Create routes with JSX elements

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import Root from './routes/Root';
 import Login from './routes/Login';
 import ErrorPage from './routes/Error';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />,
-    errorElement: <ErrorPage />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Login />} errorElement={<ErrorPage />}></Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
