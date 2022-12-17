2ad63136152d66847b02a67bcd36b2a5ccc46873
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-17 14:18:34 +0100

    Setup basic error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Home from './routes/Home';
+import ErrorPage from './routes/Error';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
