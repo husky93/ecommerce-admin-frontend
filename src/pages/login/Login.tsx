76f3ce45767537c07b59ba29e51202294ad71877
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-17 14:02:15 +0100

    Create initial files for login page

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../assets/styles/login/Login.module.css';
+
+interface LoginProps {}
+
+const Login: React.FC<LoginProps> = ({}) => {
+  return <></>;
+};
+
+export default Login;
