80e0dc9a2abddcf1bb39c2a7ce6fdd5631a0cd50
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-18 11:03:37 +0100

    Create Spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../assets/styles/components/Spinner.module.css';
+
+const Spinner: React.FC = ({}) => {
+  return (
+    <svg className={styles.spinner} viewBox="0 0 50 50">
+      <circle
+        className={styles.path}
+        cx="25"
+        cy="25"
+        r="20"
+        fill="none"
+        stroke-width="5"
+      ></circle>
+    </svg>
+  );
+};
+
+export default Spinner;
