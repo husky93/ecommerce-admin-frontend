fae9a0c66a04a575bb5c18e313f058675998f366
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-20 21:23:09 +0100

    Create css module for Dashboard route, add basic HTML template

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import styles from '../assets/styles/routes/Dashboard.module.css';
 
 interface DashboardProps {}
 
 const Dashboard: React.FC<DashboardProps> = ({}) => {
-  return <></>;
+  return (
+    <main>
+      <aside></aside>
+      <section></section>
+    </main>
+  );
 };
 
 export default Dashboard;
