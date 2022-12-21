67b533a24b2726b28697e60b15851c64771addf7
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-21 14:24:23 +0100

    Display fetched transactions

--- src/routes/Transactions.tsx
+++ src/routes/Transactions.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import styles from '../assets/styles/routes/Transactions.module.css';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { getTransactions } from '../app/api/api';
+import { useStateContext } from '../context';
 
 const Transactions: React.FC = ({}) => {
-  return <></>;
+  const { state } = useStateContext();
+
+  const { isLoading, isError, data, error } = useQuery('transactions', () =>
+    getTransactions(state.authUser?.token)
+  );
+  return (
+    <div>
+      {isLoading && <Spinner />}
+      {isError && <span>Error: {error.message}</span>}
+      {data && (
+        <div>
+          {data.map((transaction) => (
+            <Link to={`/dashboard/transactions/${transaction._id}`}>
+              {transaction._id}
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Transactions;
