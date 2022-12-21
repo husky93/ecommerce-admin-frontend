c200305bf15c10e2cd3018d337d6f5137e9e0402
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-21 11:36:18 +0100

    Finish MenuLink component

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from '../assets/styles/components/MenuLink.module.css';
 
-export const MenuLink: React.FC = ({}) => {
-  return <li></li>;
+interface MenuLinkProps {
+  children: React.ReactNode;
+  to: string;
+}
+
+export const MenuLink: React.FC<MenuLinkProps> = ({ children, to }) => {
+  return (
+    <li className={styles.item}>
+      <Link to={to} className={styles.link}>
+        {children}
+      </Link>
+    </li>
+  );
 };
