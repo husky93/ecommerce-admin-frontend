521b954794195c8211e1ba8d32bbee090733dc43
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-21 12:51:39 +0100

    Increase time before fetched data goes stale

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      staleTime: 5 * 1000,
+      staleTime: 5 * 10000,
     },
   },
 });
