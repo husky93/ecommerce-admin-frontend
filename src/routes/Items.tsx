5db536d7846fcc016fbaeaa0f3c613d7348cd8ce
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-21 12:59:42 +0100

    Fetch Items list from API with React Query

--- src/routes/Items.tsx
+++ src/routes/Items.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import styles from '../assets/styles/routes/Items.module.css';
+import Spinner from '../components/Spinner';
+import { Link } from 'react-router-dom';
+import { getItems } from '../app/api/api';
+import { useQuery } from 'react-query';
 
 const Items: React.FC = ({}) => {
-  return <></>;
+  const { isLoading, isError, data, error } = useQuery('items', getItems);
+
+  return (
+    <div className={styles.items}>
+      {isLoading && <Spinner />}
+      {isError && <span>Error: {error.message}</span>}
+      {data && (
+        <div>
+          {data.map((item) => (
+            <Link to={`/dashboard/items/${item._id}`}>{item.title}</Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Items;
