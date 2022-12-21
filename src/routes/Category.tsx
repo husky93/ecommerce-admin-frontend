5fd96b0ea6bb81b039256837a6aee371dcbaa0c7
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-21 12:38:43 +0100

    Create files for single category route

---
+++ src/routes/Category.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../assets/styles/routes/Category.module.css';
+import Spinner from '../components/Spinner';
+import { useQuery } from 'react-query';
+
+const Category: React.FC = ({}) => {
+  return <div className={styles.category}></div>;
+};
+
+export default Category;
