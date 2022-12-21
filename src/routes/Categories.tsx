86a1618eb7eb9327bb188475b1a66e18bba1dbdc
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-21 12:05:11 +0100

    Fetch Categories using React Query

--- src/routes/Categories.tsx
+++ src/routes/Categories.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import styles from '../assets/styles/routes/Categories.module.css';
+import Spinner from '../components/Spinner';
+import { useQuery } from 'react-query';
+import { getCategories } from '../app/api/api';
 
 const Categories: React.FC = ({}) => {
-  return <></>;
+  const { isLoading, isError, data, error } = useQuery(
+    'categories',
+    getCategories
+  );
+
+  return (
+    <div className={styles.categories}>
+      {isLoading && <Spinner />}
+      {isError && <span>Error: {error.message}</span>}
+      {data && (
+        <div>
+          {data.map((category) => (
+            <div>{category.title}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Categories;
