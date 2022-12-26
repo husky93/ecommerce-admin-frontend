b7091ae7ec8638d44131ed423a99c0140f8a1669
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-26 20:06:19 +0100

    Create Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logoImage from '../assets/images/logo.png';
+import styles from '../assets/styles/components/Logo.module.css';
+
+interface LogoProps {
+  text: string;
+}
+
+const Logo: React.FC<LogoProps> = ({ text }) => {
+  return (
+    <div className={styles.container}>
+      <img src={logoImage} className={styles.logo} />
+      <h1>{text}</h1>
+    </div>
+  );
+};
+
+export default Logo;
