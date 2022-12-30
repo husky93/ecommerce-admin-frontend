ef0b6a9ffe93162d90a217a3fd4baf1a31cda6d4
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-30 12:01:42 +0100

    Style UI section of Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import styles from '../assets/styles/components/NavBar.module.css';
+import profileImg from '../assets/images/profile.webp';
+import { useStateContext } from '../context';
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
-  return <nav className={styles.nav}></nav>;
+  const { state } = useStateContext();
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.ui}>
+        <span
+          className={styles.name}
+        >{`${state.authUser?.user.name} ${state.authUser?.user.surname}`}</span>
+        <img
+          alt="Profile picture"
+          className={styles.profile}
+          src={profileImg}
+        />
+      </div>
+    </nav>
+  );
 };
 export default NavBar;
