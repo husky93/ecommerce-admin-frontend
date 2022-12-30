83cd7027092d866a9c240697b2924346e9fdaec4
Maciej Sroka <sroka.maciej93@gmail.com>
2022-12-30 14:50:27 +0100

    Add spinner to Homepage while data is loading

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,21 +1,43 @@
 import React from 'react';
 import Card from '../components/Card';
+import Spinner from '../components/Spinner';
 import styles from '../assets/styles/routes/Home.module.css';
+import { useQuery } from 'react-query';
+import { getTransactions } from '../app/api/api';
+import { useStateContext } from '../context';
+import type { AxiosError } from 'axios';
 
 const Home: React.FC = ({}) => {
+  const { state } = useStateContext();
+  const { isLoading, isError, data, error } = useQuery('transactions', () =>
+    getTransactions(state.authUser?.token)
+  );
+
   return (
     <section className={styles.home} aria-label="Dashboard Home">
-      <div className={styles.dashboard_top} aria-label="Dashboard Top">
-        <Card title="Sales" variant="primary">
-          Sales: 345$
-        </Card>
-        <Card title="Sales" variant="secondary">
-          Sales: 345$
-        </Card>
-        <Card title="Sales" variant="tertiary">
-          Sales: 345$
-        </Card>
-      </div>
+      {isLoading && (
+        <div className={styles.loading}>
+          <Spinner />
+        </div>
+      )}
+      {isError && (
+        <div className={styles.dashboard_top}>
+          <span>{(error as AxiosError).message}</span>
+        </div>
+      )}
+      {data && (
+        <div className={styles.dashboard_top} aria-label="Dashboard Top">
+          <Card title="Sales" variant="primary">
+            Sales: 345$
+          </Card>
+          <Card title="Sales" variant="secondary">
+            Sales: 345$
+          </Card>
+          <Card title="Sales" variant="tertiary">
+            Sales: 345$
+          </Card>
+        </div>
+      )}
     </section>
   );
 };
