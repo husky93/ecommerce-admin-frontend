c194103db7c2ddbd673c12e27bd778f5d9459d9c
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-01 16:32:28 +0100

    Add a small variant to Logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,13 +4,17 @@ import styles from '../assets/styles/components/Logo.module.css';
 
 interface LogoProps {
   text: string;
+  sm?: boolean;
 }
 
-const Logo: React.FC<LogoProps> = ({ text }) => {
+const Logo: React.FC<LogoProps> = ({ text, sm }) => {
+  const smStyles = sm ? { fontSize: '1.15rem' } : undefined;
   return (
     <div className={styles.container}>
       <img src={logoImage} className={styles.logo} />
-      <h1 className={styles.text}>{text}</h1>
+      <h1 className={styles.text} style={smStyles}>
+        {text}
+      </h1>
     </div>
   );
 };
