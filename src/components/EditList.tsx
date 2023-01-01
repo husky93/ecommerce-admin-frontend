4e75a682044659de2d8cbb60ce845d0d4f258167
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-01 17:37:54 +0100

    Move List with edit option to separate component

---
+++ src/components/EditList.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Spinner from './Spinner';
+import styles from '../assets/styles/components/EditList.module.css';
+import { FiEdit } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import type { CategoriesGet } from '../app/api/types';
+import type { AxiosError } from 'axios';
+
+interface EditListProps {
+  isLoading: boolean;
+  isError: boolean;
+  data: CategoriesGet | undefined;
+  error: unknown;
+  name: string;
+}
+
+const EditList: React.FC<EditListProps> = ({
+  isLoading,
+  isError,
+  data,
+  error,
+  name,
+}) => {
+  return (
+    <>
+      {isLoading && <Spinner />}
+      {isError && (
+        <span>
+          {(error as AxiosError).response?.status === 404
+            ? `No ${name} found.`
+            : `Error: ${(error as AxiosError).message}`}
+        </span>
+      )}
+      {data && (
+        <div>
+          {data.map((elem) => (
+            <div key={elem._id}>
+              {elem.title}
+              <Link to={`/dashboard/${name}/${elem._id}`}>
+                <button className={styles.btn}>
+                  <FiEdit /> Edit
+                </button>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+export default EditList;
--- src/routes/Categories.tsx
+++ src/routes/Categories.tsx
@@ -1,49 +1,28 @@
 import React from 'react';
 import styles from '../assets/styles/routes/Categories.module.css';
-import Spinner from '../components/Spinner';
 import CreateButton from '../components/CreateButton';
+import EditList from '../components/EditList';
 import { useQuery } from 'react-query';
-import { Link } from 'react-router-dom';
 import { getCategories } from '../app/api/api';
-import { FiEdit } from 'react-icons/fi';
-import type { AxiosError } from 'axios';
 
 const Categories: React.FC = ({}) => {
   const { isLoading, isError, data, error } = useQuery(
     'categories',
-    getCategories,
-    {
-      initialData: [],
-    }
+    getCategories
   );
 
   return (
     <div className={styles.categories}>
       <div className={styles.ui}>
         <CreateButton to="/dashboard/categories/new" />
+        <EditList
+          isLoading={isLoading}
+          isError={isError}
+          data={data}
+          error={error}
+          name="categories"
+        />
       </div>
-      {isLoading && <Spinner />}
-      {isError && (
-        <span>
-          {(error as AxiosError).response?.status === 404
-            ? 'No categories found'
-            : `Error: ${(error as AxiosError).message}`}
-        </span>
-      )}
-      {data && (
-        <div>
-          {data.map((category) => (
-            <div key={category._id}>
-              {category.title}
-              <Link to={`/dashboard/categories/${category._id}`}>
-                <button className={styles.btn}>
-                  <FiEdit /> Edit
-                </button>
-              </Link>
-            </div>
-          ))}
-        </div>
-      )}
     </div>
   );
 };
