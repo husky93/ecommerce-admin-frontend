481e084f45331bee196dd6f43bf183ad2aaafe3f
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-01 16:19:38 +0100

    Add icons to Menu links

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import styles from '../assets/styles/components/Menu.module.css';
 import MenuLink from './MenuLink';
+import {
+  FiHome,
+  FiFolder,
+  FiShoppingBag,
+  FiShoppingCart,
+} from 'react-icons/fi';
 
 const Menu: React.FC = ({}) => {
   return (
     <nav className={styles.menu}>
       <ul>
-        <MenuLink to="/dashboard/">Dashboard</MenuLink>
-        <MenuLink to="/dashboard/categories">Categories</MenuLink>
-        <MenuLink to="/dashboard/items">Items</MenuLink>
-        <MenuLink to="/dashboard/transactions">Transactions</MenuLink>
+        <MenuLink to="/dashboard/">
+          <FiHome /> Dashboard
+        </MenuLink>
+        <MenuLink to="/dashboard/categories">
+          <FiFolder /> Categories
+        </MenuLink>
+        <MenuLink to="/dashboard/items">
+          <FiShoppingBag /> Items
+        </MenuLink>
+        <MenuLink to="/dashboard/transactions">
+          <FiShoppingCart /> Transactions
+        </MenuLink>
       </ul>
     </nav>
   );
