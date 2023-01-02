051746c321ea7f3a176bfeee2ce9680f155b82db
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-02 16:36:02 +0100

    Make side menu responsive

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from '../components/Menu';
 import NavBar from '../components/NavBar';
 import Logo from '../components/Logo';
 import styles from '../assets/styles/routes/Dashboard.module.css';
+import Hamburger from '../components/Hamburger';
 import { Outlet } from 'react-router-dom';
 
 interface DashboardProps {}
 
 const Dashboard: React.FC<DashboardProps> = ({}) => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const toggleMenu: React.MouseEventHandler<HTMLButtonElement> = () => {
+    setIsMenuActive((prevState) => !prevState);
+  };
   return (
     <main className={styles.main}>
-      <aside className={styles.aside}>
+      <Hamburger handleClick={toggleMenu} />
+      <aside className={`${styles.aside} ${isMenuActive ? styles.active : ''}`}>
         <Logo text="Shop" sm />
         <Menu />
       </aside>
