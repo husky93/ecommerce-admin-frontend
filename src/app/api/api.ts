f2f3882885cce44a8db5c3ff0fd62bbd7b1e979a
Maciej Sroka <sroka.maciej93@gmail.com>
2023-01-18 11:08:12 +0100

    Update PUT item API call function to send request with form-data

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -116,8 +116,16 @@ export const putItem = async (
   updatedItem: ItemInput
 ) => {
   if (id && token) {
-    const response = await authApi.put<Item>(`items/${id}`, updatedItem, {
-      headers: { Authorization: `Bearer ${token}` },
+    const bodyFormData = new FormData();
+    for (const [key, value] of Object.entries(updatedItem)) {
+      if (key === 'cover_img') bodyFormData.append(key, value[0]);
+      else bodyFormData.append(key, value);
+    }
+    const response = await authApi.put<Item>(`items/${id}`, bodyFormData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data',
+      },
     });
     return response.data;
   }
